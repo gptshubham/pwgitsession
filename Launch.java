f1fa72b463275db71effc4abbf597be1b2f4397e
ShubhamKrGupta <shubhamca76@gmail.com>
2023-05-27 20:19:57 +0530

    Launch.java is modified

--- Launch.java
+++ Launch.java
@@ -2,6 +2,10 @@ class Launch
 {
    void m1()
    {
-       //some code
+       system.out.println("Hello Java Lowers!");
+   }
+   void m2()
+   {
+       system.out.println("Hello Java Lowers!");
    }
 }
